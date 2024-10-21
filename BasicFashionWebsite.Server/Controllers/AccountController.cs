262a169d1ef32f31d9a79fc3aa8270e3efe6133a
Test_repo <22520992@gm.uit.edu.vn>
2024-10-21 18:43:49 +0700

    Create AccountController with CRUD

--- BasicFashionWebsite.Server/Controllers/AccountController.cs
+++ BasicFashionWebsite.Server/Controllers/AccountController.cs
@@ -2,13 +2,14 @@
 using BasicFashionWebsite.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.EntityFrameworkCore;
 
 namespace BasicFashionWebsite.Server.Controllers
 {
     [Route("Account")]
     public class AccountController : Controller
-    {   
+    {
         private readonly FashionDbContext _context;
 
         public AccountController(FashionDbContext context)
@@ -18,10 +19,96 @@ namespace BasicFashionWebsite.Server.Controllers
 
         // GET: Account
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+        public async Task<ActionResult<IEnumerable<Account>>> GetAllAccounts()
         {
             var accounts = await _context.accounts.ToListAsync();
             return Ok(accounts);
+        }
+
+        // GET: account/find-by-id?id=1
+        [HttpGet("find-by-id")]
+        public async Task<ActionResult<Account>> GetAccountById([FromQuery] int id)
+        {
+            var account = await _context.accounts.FindAsync(id);
+
+            if (account == null)
+                return NotFound();
+
+            return account;
+        }
+
+        //GET: account/find-by-username?username=cus1
+        [HttpGet("find-by-username")]
+        public async Task<ActionResult<Account>> GetAccountByUsername([FromQuery] string username)
+        {
+            var account = await _context.accounts.FirstOrDefaultAsync(ac => ac.username.Equals(username));
+            if (account == null)
+                return NotFound("Username invalid");
+
+            return account;
+        }
+
+        //POST: account/create
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateNewAccount([FromBody] Account account)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.accounts.Add(account);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAccountById), new { id = account.account_id }, account);
+        }
+
+        //DELETE: account/delete-by-id?id=1
+        [HttpDelete("delete-by-id")]
+        public async Task<ActionResult> DeleteAccountById([FromQuery] int id)
+        {
+            var account = await _context.accounts.FindAsync(id);
+
+            if (account == null)
+                return NotFound("Invalid account id");
+
+            _context.accounts.Remove(account);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        //DELETE: account/delete-by-username?username=us1
+        [HttpDelete("delete-by-username")]
+        public async Task<ActionResult> DeleteAcccountById([FromQuery] string username)
+        {
+            var deleteAccount = _context.accounts.FirstOrDefault(account => account.username.Equals(username));
+            
+            if (deleteAccount == null)
+                return NotFound();
+            
+            _context.accounts.Remove(deleteAccount);
+            await _context.SaveChangesAsync();
+            
+            return NoContent();
+        }
+
+        //PUT: account/update-by-id?id=1
+        [HttpPut("update-by-id")]
+        public async Task<ActionResult<Account>> UpdateAccountById([FromQuery] int id, [FromBody] Account account)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != account.account_id)
+                return BadRequest("Invalid account_id");
+
+            var realAccount = await _context.accounts.FindAsync(id);
+            
+            if (realAccount == null)
+                return NotFound();
+
+            _context.Entry(realAccount).CurrentValues.SetValues(account);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
--- basicfashionwebsite.client/src/App.tsx
+++ basicfashionwebsite.client/src/App.tsx
@@ -9,34 +9,56 @@ interface Account {
 }
 
 function App() {
-    const [accounts, setAccounts] = useState<Account[]>([]);
-
+    const [accounts, setAccounts] = useState<Account[] | Account>([]);
     useEffect(() => {
         populateAccountData();
     }, []);
 
-    const contents = accounts.length === 0
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Account ID</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Role</th>
-                </tr>
-            </thead>
-            <tbody>
-                {accounts.map(account =>
-                    <tr key={account.account_id}>
-                        <td>{account.account_id}</td>
-                        <td>{account.username}</td>
-                        <td>{account.password}</td>
-                        <td>{account.role}</td>
+    const renderAccountRow = (account: Account) => (
+        <tr key={account.account_id}>
+            <td>{account.account_id}</td>
+            <td>{account.username}</td>
+            <td>{account.password}</td>
+            <td>{account.role}</td>
+        </tr>
+    );
+
+    const contents = Array.isArray(accounts) ? (
+        accounts.length === 0 ? (
+            <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
+        ) : (
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Account ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Role</th>
                     </tr>
-                )}
-            </tbody>
-        </table>;
+                </thead>
+                <tbody>
+                    {accounts.map(account => renderAccountRow(account))}
+                </tbody>
+            </table>
+        )
+    ) : (
+        <div>
+            <h3>Account Details</h3>
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Account ID</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderAccountRow(accounts as Account)}
+                </tbody>
+            </table>
+        </div>
+    );
 
     return (
         <div>
@@ -47,7 +69,7 @@ function App() {
     );
 
     async function populateAccountData() {
-        const response = await fetch('account'); 
+        const response = await fetch('/account/find-by-id?id=1');
         const data = await response.json();
         setAccounts(data);
     }
